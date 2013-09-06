e27361b48568dba676fa3f3e18cd4870871a9ac0
frankV <fvalcarcel@me.com>
2013-09-06 18:29:39 -0400

    updates .gitignore, README, scrape-eps and seasons data file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 The [Br]eaking [Ba]d Timeline
 =============
+
+####This project aims to create a visual timeline representation of AMC's Breaking Bad.
+
+##Episode Data Collected From:
+Data for Epsiodes up to Season 5 - Part 1
+<br>[A Year in the Life of Walter White: A Breaking Bad Timeline](http://blog.thegreenfieldgroup.org/a-year-in-the-life-of-walter-white-a-breaking-bad-timeline/)
+<br>by Sean Aranda
+
+Data for Episodes up to Season 3
+<br>[How Much Time Has Passed on Breaking Bad?](http://www.vulture.com/2011/07/breaking_bad_calendar.html)
+<br>by Ray Rahman
+
+Epsiode meta-data collected from [IMDb](http://imdb.com)
+<br>using [IMDbPy](http://imdbpy.sourceforge.net/)
+
+
+<br>
+##Software Resources and Dependencies
+Flask
+<br>MongoDB
+<br>IMDbPy
+<br>BeautifulSoup4
+<br>TimelineJS
+<br>Bootstrap
+
+<br>
+##Project Creators
+[Emily Morehouse](/emilyemorehouse)
+<br>
+[Frank Valcarcel](/frankv)
--- scrape-eps.py
+++ scrape-eps.py
@@ -27,7 +27,8 @@ f = open(os.path.join(cwd, args.filename), 'r')
 # lines = f.readlines()
 
 Walters_Birthday = date(1957, 9, 7)
-Walter_Turns_50 = Walters_Birthday.replace(year=Walters_Birthday.year + 50)
+Walter_Turns_50 = Walters_Birthday.replace(year = Walters_Birthday.year + 50)
+First_Calendar_Day = Walter_Turns_50
 
 print 'Walt\'s birthday = ' + str(Walters_Birthday)
 print 'Walt\'s 50th = ' + str(Walter_Turns_50)
@@ -41,6 +42,11 @@ print '\n'
 seasons = 6
 season1, season2, season3, season4, season5a, season5b = ([] for i in range(seasons))
 
+eps_start_date = First_Calendar_Day
+eps_end_date = None
+weekDelta = 0
+dayDelta = 0
+
 
 """
 parsing html
@@ -50,8 +56,8 @@ parsing html
 html = bs4(f)
 
 # parsing
-if html.find('div', attrs={'class' : 'parbase section entrytext'}):
-	sections = html.findAll('div', attrs={'class' : 'parbase section entrytext'})
+if html.find('p', attrs={'class' : 'eps'}):
+	sections = html.findAll('p', attrs={'class' : 'eps'})
 	for section in sections:
 		# print section
 		if '<strong>' in str(section):
@@ -61,24 +67,55 @@ if html.find('div', attrs={'class' : 'parbase section entrytext'}):
 			# print title
 
 			eps_title = re.search(r'\"(.+?)\"', title)
+			
 			eps_season = re.search(r'(Season [0-9]+)', title)
 			eps_number = re.search(r'(Episode [0-9]+)', title)
+			season_id = re.search(r'[0-9]+', eps_season.group(0))
+			episode_id = re.search(r'[0-9]+', eps_number.group(0))
 			eps_tdelta = re.search(r'\((.*?)\)', title)
 
-			eps_tdeltaWeek = re.search(r'([0-9]+ week?)', str(eps_tdelta.group(0)))
-			# s = str(eps_tdeltaWeek)
-			# eps_tdeltaWeek = [for s in str.split() if s.isdigit()]
+			eps_tdeltaWeek = re.search(r'([0-9]+ week?)', eps_tdelta.group(0))
+			if eps_tdeltaWeek:
+				eps_tdeltaWeek = re.search(r'[0-9]+', eps_tdeltaWeek.group(0))
+				weeks = int(eps_tdeltaWeek.group(0))
+				weekDelta = timedelta(weeks = weeks)
+			
+			eps_tdeltaDay = re.search(r'([0-9]+ day?)', eps_tdelta.group(0))
+			if eps_tdeltaDay:
+				eps_tdeltaDay = re.search(r'[0-9]+', eps_tdeltaDay.group(0))
+				days = int(eps_tdeltaWeek.group(0))
+				dayDelta = timedelta(days = days)
 
-			eps_tdeltaDay = re.search(r'([0-9]+ day?)', str(eps_tdelta.group(0)))
+			print season_id.group(0) + "." + episode_id.group(0) + ":",
+			print eps_title.group(0),
+			# print eps_season.group(0) + ", " + eps_number.group(0) + " ",
+			print eps_tdelta.group(0) + '\t',
 
-			print eps_title.group(0)
-			print eps_season.group(0) + ", " + eps_number.group(0)
-			print eps_tdelta.group(0) + ", ",
-			if eps_tdeltaWeek != None:
-				print eps_tdeltaWeek.group(0) + " ",
-			if eps_tdeltaDay != None: 
-				print eps_tdeltaDay.group(0)
-			print '\n'
+
+
+			if eps_end_date != None:
+				eps_start_date = last_eps_end_date
+			
+			# add week delta
+			if weekDelta != 0:
+				# print "add " + weekDelta.group(0) + " weeks",
+				eps_end_date = First_Calendar_Day + weekDelta
+
+				# reset weekDelta
+				weekDelta = 0
+
+			# add day delta
+			if dayDelta != 0:
+				# print "and " + dayDelta.group(0) + " days"
+				eps_end_date = First_Calendar_Day + dayDelta
+
+				# reset dayDelta
+				dayDelta = 0
+
+			print "Calendar Duration: " + str(eps_start_date) + " to " + str(eps_end_date)
+
+			# set holder for this eps end date
+			last_eps_end_date = eps_end_date
 
 # print season1
 
